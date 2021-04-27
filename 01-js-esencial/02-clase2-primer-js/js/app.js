63ebceb598beae980bdc4f3ccf4b93806945839e
Pablo de los Santos <estudiodedalo@gmail.com>
2021-04-27 01:44:19 -0300

    Uso de console.log, comentarios u use strict

--- 01-js-esencial/02-clase2-primer-js/js/app.js
+++ 01-js-esencial/02-clase2-primer-js/js/app.js
@@ -1,1 +1,12 @@
-alert('Hola desde un archivo externo');
+"use strict"
+
+// alert('Hola desde un archivo externo'); comentario de linea
+
+// Ese es un mensaje de la consola
+console.log('Hola consola');
+
+var x;
+
+x = '3.1416';
+
+var publicData = "Hola"
