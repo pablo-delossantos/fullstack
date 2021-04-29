7b46e22c16902bf7c95979340b53c497d8e2e0a6
Pablo de los Santos <estudiodedalo@gmail.com>
2021-04-29 02:08:28 -0300

    Operadores aritmeticos

---
+++ 01-js-esencial/09-operadores-aritmeticos/js/app.js
@@ -0,0 +1,40 @@
+'use strict'
+
+//*** Operadores aritméticos
+
+var datoA = 10;
+var datoB = 20;
+
+// SUMA +
+var suma = datoA + datoB;
+console.log('La suma de '+ datoA + ' + '+ datoB +' es igual a: ', suma);
+
+// RESTA -
+var resta = datoA - datoB;
+console.log('La resta de '+ datoA + ' + '+ datoB +' es igual a: ', resta);
+
+// MULTIPLICACION *
+var multiplicacion = datoA * datoB;
+console.log('La multiplicación de '+ datoA + ' + '+ datoB +' es igual a: ', multiplicacion);
+
+// DIVISION /
+var division = datoA / datoB;
+console.log('La división de '+ datoA + ' + '+ datoB +' es igual a: ', division);
+
+// MODULO O RESIDUO %
+var modulo = datoA % datoB;
+console.log('La módulo de '+ datoA + ' + '+ datoB +' es igual a: ', modulo);
+
+//INCREMENTO ++
+var incremento = datoA;
+incremento++;
+
+incremento = incremento+1
+console.log('El incremento ++ de ' + datoA + ' es igual a:', incremento)
+
+//DECREMENTO --
+var decremento = datoA;
+decremento--;
+
+decremento = decremento-1
+console.log('El decremento ++ de ' + datoA + ' es igual a:', decremento)
