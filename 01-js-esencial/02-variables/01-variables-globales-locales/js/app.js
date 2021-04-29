09ec530143be24f5c347c0e23da4349cffc68d3b
Pablo de los Santos <estudiodedalo@gmail.com>
2021-04-28 23:54:56 -0300

    Variables locales y variables globales

---
+++ 01-js-esencial/02-variables/01-variables-globales-locales/js/app.js
@@ -0,0 +1,15 @@
+'Use strict'
+
+var nombre = 'Sergio';
+
+console.log(nombre)
+
+function saludo() {
+  let nombre = 'yacafx';
+  console.log(nombre)
+
+  let edad = 34;
+  console.log(edad)
+}
+
+saludo();
