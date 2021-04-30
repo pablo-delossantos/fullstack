7aa2ce26dcb8b0488edac0708e33b476b02e65ca
Pablo de los Santos <estudiodedalo@gmail.com>
2021-04-30 16:22:41 -0300

    operadores relacionales

---
+++ 01-js-esencial/10-operadores-relacionales/js/app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// Operadores relacionaleS
+
+var datoA = 10;
+var datoB = 20;
+
+// MAYOR QUE >
+var = mayorQue = datoA > datoB;
+console.log("Es "+ datoA + "mayor que " + datoB + ": " mayorQue);
+
+// MENOR QUE <
+var = menorQue = datoA;
+
+// MAYOR O IGUAL QUE >=
+var = mayorOIgualQue = datoA;
+
+// MENOR O IGUAL QUE <=
+var = menorOIgualQue = datoA;
+
+// IGUAL QUE ==
+var = igualQue = datoA;
+
+// NO ES IGUAL QUE O ES DIFERENTE !=
+var = noEsIgualQue = datoA;
+
