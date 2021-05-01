45138a282158e08d4a6d77068bb8a6497a49ac07
Pablo de los Santos <estudiodedalo@gmail.com>
2021-05-01 18:52:14 -0300

    Operadores de concatenacion

---
+++ 01-js-esencial/14-operador-de-concatenacion/js/app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// Operadores de concatenacion
+var datoA = 10;
+var datoB = 20;
+
+var nombre = 'Sergio';
+var apellido = 'Brito';
+
+// CONCATENACIÓN DE NÚMEROS
+var concatNumeros = datoA + datoB;
+console.log("Los numeros " + datoA + " y " + datoB + " usados como números concatenados hacen una suma igual a: " + concatNumeros)
+
+// CONCATENACIÓN DE CADENAS DE TEXTO
+var concatTexto = nombre + " " + apellido;
+console.log("Los textos " + nombre + " y " + apellido + " se unen para formar: " + concatTexto);
+
+// CONCATENACIÓN DE NÚMEROS COMO TEXTO
+var concatNumComoTxt = "2" + "8";
+console.log("Los números 2 y 8 usados como texto se unen para formar: ", concatNumComoTxt);
+
+// CONCATENACIÓN DE TEXTO Y NUMERO
+var concatTxtNum = datoA + "8";
+console.log("El número "+ datoA +" usado como número y 8 como texto se unen para formar: " + concatTxtNum);
