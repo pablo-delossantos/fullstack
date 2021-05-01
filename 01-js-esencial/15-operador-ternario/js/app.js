151242de66f499382d7629f6ad4bb20ad06bb25d
Pablo de los Santos <estudiodedalo@gmail.com>
2021-05-01 20:59:35 -0300

    Operador ternario o condicional

---
+++ 01-js-esencial/15-operador-ternario/js/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// Operador ternario o condicional
+
+var datoA = 10;
+var datoB = 20;
+
+// Condición ? TRUE : FALSE
+var resultado = datoA > datoB ? "Si es mayor" : "No es mayor";
+
+console.log("El resultado con el operador ternario u operador condicional es: ", resultado);
