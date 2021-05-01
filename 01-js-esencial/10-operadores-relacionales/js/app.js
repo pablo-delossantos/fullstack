08b815faf52087c81097d59f2d8a0ca6370a3e82
Pablo de los Santos <estudiodedalo@gmail.com>
2021-04-30 23:36:53 -0300

    Validacion conOperadores relacionales

--- 01-js-esencial/10-operadores-relacionales/js/app.js
+++ 01-js-esencial/10-operadores-relacionales/js/app.js
@@ -6,21 +6,25 @@ var datoA = 10;
 var datoB = 20;
 
 // MAYOR QUE >
-var = mayorQue = datoA > datoB;
-console.log("Es "+ datoA + "mayor que " + datoB + ": " mayorQue);
+var mayorQue = datoA > datoB;
+console.log("Es "+ datoA + " mayor que " + datoB + ": " + mayorQue);
 
 // MENOR QUE <
-var = menorQue = datoA;
+var menorQue = datoA < datoB;
+console.log("Es "+ datoA + " menor que " + datoB + ": " + menorQue);
 
 // MAYOR O IGUAL QUE >=
-var = mayorOIgualQue = datoA;
+var mayorOIgualQue = datoA >= datoB;
+console.log("Es "+ datoA + " mayor o igual que " + datoB + ": " + mayorOIgualQue);
 
 // MENOR O IGUAL QUE <=
-var = menorOIgualQue = datoA;
+var menorOIgualQue = datoA <= datoB;
+console.log("Es "+ datoA + " menor o igual que " + datoB + ": " + menorOIgualQue);
 
 // IGUAL QUE ==
-var = igualQue = datoA;
+var igualQue = datoA == datoB;
+console.log("Es "+ datoA + " igual que " + datoB + ": " + igualQue);
 
 // NO ES IGUAL QUE O ES DIFERENTE !=
-var = noEsIgualQue = datoA;
-
+var noEsIgualQue = datoA != datoB;
+console.log("No "+ datoA + " igual o es diferente que " + datoB + ": " + noEsIgualQue);
