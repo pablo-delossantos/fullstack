584fbd3a85452b45a907e7bfc05dfd37a5d42e07
Pablo de los Santos <estudiodedalo@gmail.com>
2021-05-01 22:12:56 -0300

    Condicional if-else

---
+++ 01-js-esencial/18-if-else/js/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// Condición IF
+
+var datoA = 10;
+var datoB = 20;
+var resultado = "Sin datos";
+
+if (datoA > datoB){
+  resultado = "La condición se cumplió";
+} else {
+  resultado = "La condición no se cumplió"
+}
+
+console.log("El resultado de la evaluación if es:", resultado);
