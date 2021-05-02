ecffac47a4f43648107e5ebf3e894420cd530284
Pablo de los Santos <estudiodedalo@gmail.com>
2021-05-02 01:16:05 -0300

    Cliclo FOR

---
+++ 01-js-esencial/22-ciclo-for/js/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// Ciclos
+
+// Clclos | Loops | Iteradores
+
+// 2 tipos: Definidos e indefinidos
+
+// Definidos: Ciclo FOR
+
+// indefinidos: Ciclo WHILE y Ciclo DO...WHILE
+// ***********************
+
+// Ciclo FOR | Ciclo Definido
+// Repetición = iteración
+
+// COMPONENTES DE UN CICLO FOR
+// Contador
+// Condición o evaluación
+// Paso | incrementador o decrementador
+
+var productos = 5;
+
+for (let contador = 0; contador < productos; contador++) {
+  console.log('Producto #'+ contador);
+  debugger;
+}
