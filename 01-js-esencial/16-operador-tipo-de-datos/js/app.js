9797292b8a51dfc306aced4da59d3f4d0232e983
Pablo de los Santos <estudiodedalo@gmail.com>
2021-05-01 21:14:43 -0300

    Operador de tipo de datos typeof

---
+++ 01-js-esencial/16-operador-tipo-de-datos/js/app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// Operador de tipo de datos
+
+var datoA = 10;
+
+var nombre = 'Playa'; // Cadena de texto
+
+var activo = true // Boleano
+
+var persona = {
+  edad: 34, // Número
+  deporte: 'Correr' // Cadena de texto
+} // Objeto
+
+console.log(typeof datoA);
+console.log(typeof nombre);
+console.log(typeof activo);
+
+console.log(typeof persona);
+
+console.log(typeof persona.edad);
+console.log(typeof persona.deporte);
