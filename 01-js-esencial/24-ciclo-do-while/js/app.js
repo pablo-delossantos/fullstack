ac464939ea77d51310daa366282b32dff426595f
Pablo de los Santos <estudiodedalo@gmail.com>
2021-05-02 01:34:25 -0300

    Ciclo Do While

---
+++ 01-js-esencial/24-ciclo-do-while/js/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Ciclo Do..WHILE | Ciclo Indefinido
+// Iteración indeterminada o desconocida
+
+var productos = 5;
+
+do {
+  console.log('Producto vendido');
+  productos--;
+  debugger;
+}
+while(productos>=1)
