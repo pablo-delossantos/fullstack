eb22afbe890afdb8c8fe9dda66fc9bd90426579f
Pablo de los Santos <estudiodedalo@gmail.com>
2021-05-03 14:40:10 -0300

    Parametros REST

---
+++ 01-js-esencial/29-parametros-rest/js/app.js
@@ -0,0 +1,9 @@
+"use strict"
+
+function cocinar(...masIngredientes) {
+  // console.log("Ingrediente 1: ", ingrediente1);
+  // console.log("Ingrediente 2: ", ingrediente2);
+  console.log("mas ingredientes: ", masIngredientes);
+}
+
+cocinar("Pollo", "Tomate", "Arroz", "Frijoles", "Pescado", "Chile");
