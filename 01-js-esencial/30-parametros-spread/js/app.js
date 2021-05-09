8ea79836fc3b4c11b2b3977a18bd34e0fd696ae5
Pablo de los Santos <estudiodedalo@gmail.com>
2021-05-08 22:48:10 -0300

    Parametros spread

--- 01-js-esencial/30-parametros-spread/js/app.js
+++ 01-js-esencial/30-parametros-spread/js/app.js
@@ -2,12 +2,13 @@
 
 // Parametros SPREAD
 
-function cocinar(ingrediente1, ingrediente2, ingrediente3) {
+function cocinar(ingrediente1, ingrediente2, ingrediente3, ...otros) {
   console.log("Ingrediente1: ", ingrediente1);
   console.log("Ingrediente2: ", ingrediente2);
   console.log("Ingrediente3: ", ingrediente3);
+  console.log("Otros: " , otros)
 }
 
 var ingredientesBase = ["Pollo", "Tomate"];
 
-cocinar(ingredientesBase, "Arroz");
+cocinar(...ingredientesBase, "Arroz", "Pescado", "Chile");
