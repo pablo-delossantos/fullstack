54022308135c6d5a90969e2dade1c96cf7f75c09
Pablo de los Santos <estudiodedalo@gmail.com>
2021-05-08 22:30:59 -0300

    Parametros Spread archivos iniciales

---
+++ 01-js-esencial/30-parametros-spread/js/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Parametros SPREAD
+
+function cocinar(ingrediente1, ingrediente2, ingrediente3) {
+  console.log("Ingrediente1: ", ingrediente1);
+  console.log("Ingrediente2: ", ingrediente2);
+  console.log("Ingrediente3: ", ingrediente3);
+}
+
+var ingredientesBase = ["Pollo", "Tomate"];
+
+cocinar(ingredientesBase, "Arroz");
